132510db43c66515c55a09d28b9a2b64a4171153
Craig Booker <info@itech.plus>
2018-05-15 10:36:06 -0500

    Created project. Relocated Log statement from if else clause to the base if the try catch to adhere to the dry principle.

---
+++ app/src/main/java/com/arcadiasoftworks/stormy/MainActivity.java
@@ -0,0 +1,63 @@
+package com.arcadiasoftworks.stormy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "a371e6cef5025d37a4a1039c6d26009b";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+        
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.e(TAG, response.body().string());
+                    if (response.isSuccessful()){
+
+                    }
+                    else {
+
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught: ", e);
+                }
+            }
+        });
+
+    Log.d(TAG, "Main UI code is running, yay!");
+    }
+}
