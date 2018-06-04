4c2a3e56a369baa6762d4035fa76fe85064b641b
Craig Booker <info@itech.plus>
2018-06-04 10:49:17 -0500

    Added Forecast class to hold the Day, Hour, and Current data model classes.

---
+++ app/src/main/java/com/arcadiasoftworks/stormy/weather/Forecast.java
@@ -0,0 +1,12 @@
+package com.arcadiasoftworks.stormy.weather;
+
+/**
+ * Created by ${user} on ${date}
+ */
+public class Forecast {
+
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+    
+}
